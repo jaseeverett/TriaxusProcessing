cc2052cab59e260537405c814ade7b993e7e9252
jaseeverett <Jason.Everett@unsw.edu.au>
2021-12-02 08:16:20 +1100

    Update plotting

--- Triaxus_2D_plot.m
+++ Triaxus_2D_plot.m
@@ -19,10 +19,12 @@ cb1 = colorbar;
 colormap(h1,jet)
 hold on
 p = plot(s.grnddist,s.pressure,'color',[0.5 0.5 0.5],'linewidth',0.1);
+contour(XXI,YYI,ZZI,[20 20], 'linecolor', 'w', 'lineWidth', 1);
+contour(XXI,YYI,ZZI,[20 20], ':', 'linecolor', 'k', 'lineWidth', 1);
 
 set(gca,'ydir','r','fontsize',txt,'ytick',[50:50:maxD+50])
 set(gca,'XTickLabel','')
-tx1 = text(txt_x,txt_y,'A) Temperature','horizontalalignment','l','units','normalized','fontsize',txt-2, 'color',fc);
+tx1 = text(txt_x,txt_y,'A) Temperature','horizontalalignment','l','units','normalized','fontsize',txt-3, 'color',fc);
 ylabel('Depth (m)')
 xlim([xlim_start xlim_end])
 ylim([minD maxD])
@@ -41,11 +43,14 @@ hold on
 % p = plot(s.grnddist(1:10),s.pressure(1:10),'.k','markersize',1);
 set(gca,'ydir','r','fontsize',txt,'ytick',[50:50:maxD+50])
 set(gca,'XTickLabel','')
-tx2 = text(txt_x,txt_y,'B) Chl. \ita\rm (mg m^{-3})','horizontalalignment','l','units','normalized','fontsize',txt-2, 'color',fc);
+tx2 = text(txt_x,txt_y,'B) Chl. \ita\rm (mg m^{-3})','horizontalalignment','l','units','normalized','fontsize',txt-3, 'color',fc);
 ylim([minD maxD])
 
-[XXI,YYI,ZZI] = find_downcast(s.grnddist,s.pressure,s.rho, depth_res, minext, interpVert);
-[C h] = contour(XXI,YYI,ZZI,rho.conts,'edgecolor',[0.5 0.5 0.5]);
+contour(XXI,YYI,ZZI,[0.1 0.1], 'linecolor', 'w', 'lineWidth', 1);
+contour(XXI,YYI,ZZI,[0.1 0.1], ':', 'linecolor', 'k', 'lineWidth', 1);
+
+% [XXI,YYI,ZZI] = find_downcast(s.grnddist,s.pressure,s.rho, depth_res, minext, interpVert);
+% [C h] = contour(XXI,YYI,ZZI,rho.conts,'edgecolor',[0.5 0.5 0.5]);
 clabel(C,h,'LabelSpacing',150,'color',[0.5 0.5 0.5],'fontsize',6);
 xlim([xlim_start xlim_end])
 ylim([minD maxD])
@@ -62,7 +67,7 @@ colormap(h3,jet)
 hold on
 set(gca,'ydir','r','fontsize',txt,'ytick',[50:50:maxD+50])
 set(gca,'XTickLabel','')
-tx3 = text(txt_x,txt_y,'C) log_{10} Zooplankton Abundance (ind. m^{-3})','horizontalalignment','l','units','normalized','fontsize',txt-2, 'color',fc);
+tx3 = text(txt_x,txt_y,'C) log_{10} Zooplankton Abundance (ind. m^{-3})','horizontalalignment','l','units','normalized','fontsize',txt-3, 'color',fc);
 ylabel('Depth (m)')
 xlim([xlim_start xlim_end])
 ylim([minD maxD])
@@ -81,7 +86,7 @@ hold on
 % p = plot(s.grnddist(1:10),s.pressure(1:10),'.k','markersize',1);
 set(gca,'ydir','r','fontsize',txt,'ytick',[50:50:maxD+50])
 set(gca,'XTickLabel','')
-tx4 = text(txt_x,txt_y,'D) log_{10} Zooplankton Biomass (mg m^{-3})','horizontalalignment','l','units','normalized','fontsize',txt-2, 'color',fc);
+tx4 = text(txt_x,txt_y,'D) log_{10} Zooplankton Biomass (mg m^{-3})','horizontalalignment','l','units','normalized','fontsize',txt-3, 'color',fc);
 xlim([xlim_start xlim_end])
 ylim([minD maxD])
 
@@ -100,7 +105,7 @@ hold on
 % p = plot(s.grnddist(1:10),s.pressure(1:10),'.k','markersize',1);
 set(gca,'ydir','r','fontsize',txt,'ytick',[50:50:maxD+50])
 set(gca,'XTickLabel','')
-tx5 = text(txt_x,txt_y,'E) NBSS Linear Slope','horizontalalignment','l','units','normalized','fontsize',txt-2, 'color',fc);
+tx5 = text(txt_x,txt_y,'E) NBSS Linear Slope','horizontalalignment','l','units','normalized','fontsize',txt-3, 'color',fc);
 ylabel('Depth (m)')
 xlim([xlim_start xlim_end])
 ylim([minD maxD])
@@ -116,7 +121,7 @@ cmap = colormap(h6,jet);
 hold on
 % p = plot(s.grnddist(1:10),s.pressure(1:10),'.k','markersize',1);
 set(gca,'ydir','r','fontsize',txt,'ytick',[50:50:maxD+50])
-tx6 = text(txt_x,txt_y,'F) Geometric Mean Size (\mum)','horizontalalignment','l','units','normalized','fontsize',txt-2, 'color',fc);
+tx6 = text(txt_x,txt_y,'F) Geometric Mean Size (\mum)','horizontalalignment','l','units','normalized','fontsize',txt-3, 'color',fc);
 xlim([xlim_start xlim_end])
 ylim([minD maxD])
 
@@ -128,12 +133,12 @@ xticklabel = get(gca,'XTickLabel');
 xlat = roundn(s.latitude(dsearchn(s.grnddist,xtick')),-2);
 YT = max(get(gca,'YLim'));
 
-set(gca,'XTickLabel','')
+% set(gca,'XTickLabel','')
 
-for a = 1:length(xtick)
-    eval(['xtick_txt = {''',xticklabel{a,:},''';''(',num2str(xlat(a,:)),')''};']);
-    tx(a) = text(xtick(a),YT,xtick_txt,'horizontalalignment','center','verticalalignment','top','fontsize',txt);
-    clear xtick_txt
-end
+% for a = 1:length(xtick)
+%     eval(['xtick_txt = {''',xticklabel{a,:},''';''(',num2str(xlat(a,:)),')''};']);
+%     tx(a) = text(xtick(a),YT,xtick_txt,'horizontalalignment','center','verticalalignment','top','fontsize',txt);
+%     clear xtick_txt
+% end
 
 tx_bot = text(s.grnddist(end)/2,YT+90,'Distance along transect (km)','horizontalalignment','center','fontsize',txt);
